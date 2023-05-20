e319937a8e9ad1fbd915710ab2a7f955fcfad795
Hanson Nguyen <64661526+hansonguyen@users.noreply.github.com>
2023-05-20 16:58:59 -0700

    cors

--- api/server.js
+++ api/server.js
@@ -2,11 +2,17 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const petsRoutes = require('./routes/pets')
 
 const app = express()
 
 // Middleware
+app.use(
+    cors({
+        origin: ['http://localhost:3000']
+    })
+)
 app.use(express.json())
 app.use((req, res, next) => {
     console.log(req.path, req.method)
