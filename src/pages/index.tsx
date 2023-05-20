5c3502dbc0bd7d3b642297da328ffccc0c213abb
Naomi <naomipremlim@gmail.com>
2023-05-20 16:36:18 -0700

    trial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,36 @@
-import { createStyles, Overlay, Container, Title, Button, Text, rem, ButtonProps, Group  } from '@mantine/core';
-import Head from 'next/head';
+import {
+  createStyles,
+  Overlay,
+  Container,
+  Title,
+  Button,
+  Text,
+  rem,
+  ButtonProps,
+  Group,
+} from "@mantine/core";
+import Head from "next/head";
 
 const useStyles = createStyles((theme) => ({
   hero: {
-    position: 'relative',
+    position: "relative",
     backgroundImage:
-      'url(https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
+      "url(https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
   },
 
   container: {
     height: rem(700),
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'flex-end',
-    alignItems: 'flex-start',
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-end",
+    alignItems: "flex-start",
     paddingBottom: `calc(${theme.spacing.xl} * 6)`,
     zIndex: 1,
-    position: 'relative',
+    position: "relative",
 
-    [theme.fn.smallerThan('sm')]: {
+    [theme.fn.smallerThan("sm")]: {
       height: rem(500),
       paddingBottom: `calc(${theme.spacing.xl} * 3)`,
     },
@@ -32,12 +42,12 @@ const useStyles = createStyles((theme) => ({
     fontWeight: 900,
     lineHeight: 1.1,
 
-    [theme.fn.smallerThan('sm')]: {
+    [theme.fn.smallerThan("sm")]: {
       fontSize: rem(40),
       lineHeight: 1.2,
     },
 
-    [theme.fn.smallerThan('xs')]: {
+    [theme.fn.smallerThan("xs")]: {
       fontSize: rem(28),
       lineHeight: 1.3,
     },
@@ -47,8 +57,8 @@ const useStyles = createStyles((theme) => ({
     color: theme.white,
     maxWidth: 600,
 
-    [theme.fn.smallerThan('sm')]: {
-      maxWidth: '100%',
+    [theme.fn.smallerThan("sm")]: {
+      maxWidth: "100%",
       fontSize: theme.fontSizes.sm,
     },
   },
@@ -56,34 +66,27 @@ const useStyles = createStyles((theme) => ({
   control: {
     marginTop: `calc(${theme.spacing.xl} * 1.5)`,
 
-    [theme.fn.smallerThan('sm')]: {
-      width: '100%',
+    [theme.fn.smallerThan("sm")]: {
+      width: "100%",
     },
   },
 
-  buttonContainer:{display:'flex', justifyContent:'row', width:'100%'},
-  button:{
-    width:'20%',
-
-    },
+  buttonContainer: { display: "flex", justifyContent: "row", width: "100%" },
+  button: {
+    width: "20%",
+  },
 }));
 
-export function SitterButton(props: ButtonProps & React.ComponentPropsWithoutRef<'a'>) {
-  return (
-    <Button
-      component="a"
-      variant="default"
-      {...props}
-    />
-  );
+export function SitterButton(
+  props: ButtonProps & React.ComponentPropsWithoutRef<"a">
+) {
+  return <Button component="a" variant="default" {...props} />;
 }
 
 export default function HeroContentLeft() {
   const { classes } = useStyles();
 
   return (
-
-
     <div className={classes.hero}>
       <Head>
         <title>Pawfect Sitters</title>
@@ -96,14 +99,13 @@ export default function HeroContentLeft() {
       <Container className={classes.container}>
         <Title className={classes.title}>Pawfect Sitters</Title>
         <Text className={classes.description} size="xl" mt="xl">
-          Build fully functional accessible web applications faster than ever – Mantine includes
-          more than 120 customizable components and hooks to cover you in any situation
+          Build fully functional accessible web applications faster than ever –
+          Mantine includes more than 120 customizable components and hooks to
+          cover you in any situation
         </Text>
 
         <div className={classes.buttonContainer}>
-          <SitterButton className={classes.button}>
-            Sitter
-          </SitterButton>
+          <SitterButton className={classes.button}>Sitters</SitterButton>
         </div>
       </Container>
     </div>
