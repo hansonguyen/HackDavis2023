d8442abcd4287721fcea020dcadc725911d84053
Hanson Nguyen <64661526+hansonguyen@users.noreply.github.com>
2023-05-20 17:16:36 -0700

    auth

---
+++ components/Navbar.tsx
@@ -0,0 +1,17 @@
+import { Header, Button, rem } from "@mantine/core";
+import { useUser } from '@auth0/nextjs-auth0/client';
+
+function Navbar() {
+    const { user, error, isLoading } = useUser();
+
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>{error.message}</div>;
+    return (
+        <Header height={rem(84)}>
+            {!user && <a href="/api/auth/login">Login</a>}
+            {user && <a href="/api/auth/logout">Logout</a>}
+        </Header>
+    )
+}
+
+export default Navbar
--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,5 +1,20 @@
+import { useUser } from '@auth0/nextjs-auth0/client';
+import Navbar from '../../../components/Navbar';
+
 export default function User() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
   return (
-    <div>Hello User</div>
-  )
+    user && (
+      <div>
+        <Navbar />
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+    )
+  );
 }
