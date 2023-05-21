4a8a0dbce2d5bfe05562be2cceae4ab38ac3ad24
Naomi <naomipremlim@gmail.com>
2023-05-21 07:04:41 -0700

    landing page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,42 +1,95 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import { MantineProvider } from '@mantine/core';
-import { UserProvider } from '@auth0/nextjs-auth0/client';
+import { AppProps } from "next/app";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 
 export default function App(props: AppProps) {
-    const { Component, pageProps } = props;
+  const { Component, pageProps } = props;
 
-    return (
-        <>
-            <Head>
-                <title>Pawfect Sitters</title>
-                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-            </Head>
-            <UserProvider>
-                <MantineProvider
-                    withGlobalStyles
-                    withNormalizeCSS
-                    theme={{
-                        /** Put your mantine theme override here */
-                        colors: {
-                            'gray': ['#C1CDC1','#C1CDC1','#C1CDC1','#C1CDC1','#C1CDC1','#C1CDC1','#C1CDC1','#C1CDC1','#C1CDC1','#C1CDC1'],
-                            'yellow': ['#FFEBB9','#FFEBB9','#FFEBB9','#FFEBB9','#FFEBB9','#FFEBB9','#FFEBB9','#FFEBB9','#FFEBB9','#FFEBB9'],
-                            'orange': ['#FDD1A5','#FDD1A5' ,'#FDD1A5' ,'#FDD1A5' ,'#FDD1A5' ,'#FDD1A5' ,'#FDD1A5' ,'#FDD1A5' ,'#FDD1A5' ,'#FDD1A5' ],
-                            'green': ['#D9E7C1','#D9E7C1','#D9E7C1','#D9E7C1','#D9E7C1','#D9E7C1','#D9E7C1','#D9E7C1','#D9E7C1','#D9E7C1'],
-                            'beige': ['FAEBD7','FAEBD7','FAEBD7','FAEBD7','FAEBD7','FAEBD7','FAEBD7','FAEBD7','FAEBD7','FAEBD7'],
-                            'brand': ['#C1CDC1',
-                            '#FFEBB9',
-                            '#FDD1A5',
-                            '#D9E7C1',
-                            'FAEBD7']
-                        
-                          },
-                          primaryColor: 'orange',
-                    }}
-                    >
-                    <Component {...pageProps} />
-                </MantineProvider>
-            </UserProvider>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>Pawfect Sitters</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <UserProvider>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            /** Put your mantine theme override here */
+            colors: {
+              gray: [
+                "#C1CDC1",
+                "#C1CDC1",
+                "#C1CDC1",
+                "#C1CDC1",
+                "#C1CDC1",
+                "#C1CDC1",
+                "#C1CDC1",
+                "#C1CDC1",
+                "#C1CDC1",
+                "#C1CDC1",
+              ],
+              yellow: [
+                "#FFEBB9",
+                "#FFEBB9",
+                "#FFEBB9",
+                "#FFEBB9",
+                "#FFEBB9",
+                "#FFEBB9",
+                "#FFEBB9",
+                "#FFEBB9",
+                "#FFEBB9",
+                "#FFEBB9",
+              ],
+              orange: [
+                "#FDD1A5",
+                "#FDD1A5",
+                "#FDD1A5",
+                "#FDD1A5",
+                "#FDD1A5",
+                "#FDD1A5",
+                "#FDD1A5",
+                "#FDD1A5",
+                "#FDD1A5",
+                "#FDD1A5",
+              ],
+              green: [
+                "#D9E7C1",
+                "#D9E7C1",
+                "#D9E7C1",
+                "#D9E7C1",
+                "#D9E7C1",
+                "#D9E7C1",
+                "#D9E7C1",
+                "#D9E7C1",
+                "#D9E7C1",
+                "#D9E7C1",
+              ],
+              beige: [
+                "FAEBD7",
+                "FAEBD7",
+                "FAEBD7",
+                "FAEBD7",
+                "FAEBD7",
+                "FAEBD7",
+                "FAEBD7",
+                "FAEBD7",
+                "FAEBD7",
+                "FAEBD7",
+              ],
+              brand: ["#C1CDC1", "#FFEBB9", "#FDD1A5", "#D9E7C1", "FAEBD7"],
+            },
+            primaryColor: "orange",
+          }}
+        >
+          <Component {...pageProps} />
+        </MantineProvider>
+      </UserProvider>
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,74 @@
 import Navbar from "../../components/Navbar";
-import { Text } from '@mantine/core';
+import { Text } from "@mantine/core";
+import { createStyles } from "@mantine/core";
+import { useRouter } from "next/router";
+
+//font styles
+import { Montserrat } from "next/font/google";
+import { Inter } from "next/font/google";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
+
+const useStyles = createStyles((theme) => ({
+  container: {
+    backgroundImage: "linear-gradient(to right, #FFEBB9, white)",
+    height: "100vh",
+  },
+  title: {
+    display: "flex",
+    fontSize: 50,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    marginLeft: "10%",
+    fontWeight: 600,
+    paddingTop: "15%",
+    fontFamily: "Montserrat",
+  },
+  subtitle: {
+    display: "flex",
+    fontSize: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    fontWeight: 400,
+    marginLeft: "10%",
+    width: "60%",
+    fontFamily: "Montserrat",
+  },
+  button: {
+    backgroundColor: "#D9E7C1",
+    marginLeft: "10%",
+    marginTop: "5%",
+    padding: "15px 32px",
+    borderWidth: 1,
+    borderRadius: 50,
+  },
+}));
 
 export default function Home() {
+  const { classes } = useStyles();
+  const router = useRouter();
+  const nav = () => {
+    router.push("/petdisplay");
+  };
 
   return (
-    <div>
-      <Navbar />
-      <h1>Find your 
-pawfect sitter Today!</h1>
-
-      <Text>Going on vacation? need someone to watch over your pet? Looking to begin your journey in pet sitting? get started with us today!</Text>
+    <div className={classes.container}>
+      {/* <Navbar /> */}
+      <div className={classes.title}>
+        <Text className={montserrat.className}>
+          Find your pawfect sitter today!
+        </Text>
+      </div>
+      <div className={classes.subtitle}>
+        <Text className={inter.className}>
+          Going on vacation? Need someone to watch over your pet? Looking to
+          begin your journey in pet sitting? Get started with us today!
+        </Text>
+      </div>
+      <button className={classes.button} type="button" onClick={nav}>
+        Click Me!
+      </button>
     </div>
   );
 }
